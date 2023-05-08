f0e6d07c3411d2ed7cd383f13d2421b67498be37
Haoyu Qiu <timothyqiu32@gmail.com>
2023-05-08 15:18:10 +0800

    01: 通过相机发现更大的世界

---
+++ world.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var tile_map: TileMap = $TileMap
+@onready var camera_2d: Camera2D = $Player/Camera2D
+
+
+func _ready() -> void:
+	var used := tile_map.get_used_rect()
+	var tile_size := tile_map.tile_set.tile_size
+	
+	camera_2d.limit_top = used.position.y * tile_size.y
+	camera_2d.limit_right = used.end.x * tile_size.x
+	camera_2d.limit_bottom = used.end.y * tile_size.y
+	camera_2d.limit_left = used.position.x * tile_size.x
+	camera_2d.reset_smoothing()
