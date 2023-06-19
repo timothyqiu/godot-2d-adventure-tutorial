dbbd88886bc2d7831331c4c7cfc7d2e343d2d947
Haoyu Qiu <timothyqiu32@gmail.com>
2023-06-19 17:10:15 +0800

    04: 如何实现丝滑的运动

--- player.gd
+++ player.gd
@@ -1,24 +1,41 @@
 extends CharacterBody2D
 
-const RUN_SPEED := 200.0
-const JUMP_VELOCITY := -300.0
+const RUN_SPEED := 160.0
+const FLOOR_ACCELERATION := RUN_SPEED / 0.2
+const AIR_ACCELERATION := RUN_SPEED / 0.02
+const JUMP_VELOCITY := -320.0
 
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_request_timer: Timer = $JumpRequestTimer
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		jump_request_timer.start()
+	
+	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
+		velocity.y = JUMP_VELOCITY / 2
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
-	velocity.x = direction * RUN_SPEED
+	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
 	velocity.y += gravity * delta
 	
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	var should_jump := can_jump and jump_request_timer.time_left > 0
+	if should_jump:
 		velocity.y = JUMP_VELOCITY
+		coyote_timer.stop()
+		jump_request_timer.stop()
 	
 	if is_on_floor():
-		if is_zero_approx(direction):
+		if is_zero_approx(direction) and is_zero_approx(velocity.x):
 			animation_player.play("idle")
 		else:
 			animation_player.play("running")
@@ -28,4 +45,11 @@ func _physics_process(delta: float) -> void:
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
 	
+	var was_on_floor := is_on_floor()
 	move_and_slide()
+	
+	if is_on_floor() != was_on_floor:
+		if was_on_floor and not should_jump:
+			coyote_timer.start()
+		else:
+			coyote_timer.stop()
