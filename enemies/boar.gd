7978614174dc3d88d9d73e32c542d85817e5a1e3
Haoyu Qiu <timothyqiu32@gmail.com>
2023-09-14 23:47:52 +0800

    09: 如何实现三段攻击

--- enemies/boar.gd
+++ enemies/boar.gd
@@ -12,6 +12,12 @@ enum State {
 @onready var calm_down_timer: Timer = $CalmDownTimer
 
 
+func can_see_player() -> bool:
+	if not player_checker.is_colliding():
+		return false
+	return player_checker.get_collider() is Player
+
+
 func tick_physics(state: State, delta: float) -> void:
 	match state:
 		State.IDLE:
@@ -24,12 +30,12 @@ func tick_physics(state: State, delta: float) -> void:
 			if wall_checker.is_colliding() or not floor_checker.is_colliding():
 				direction *= -1
 			move(max_speed, delta)
-			if player_checker.is_colliding():
+			if can_see_player():
 				calm_down_timer.start()
 
 
 func get_next_state(state: State) -> State:
-	if player_checker.is_colliding():
+	if can_see_player():
 		return State.RUN
 	
 	match state:
--- player.gd
+++ player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 enum State {
@@ -8,17 +9,26 @@ enum State {
 	LANDING,
 	WALL_SLIDING,
 	WALL_JUMP,
+	ATTACK_1,
+	ATTACK_2,
+	ATTACK_3,
 }
 
-const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
+const GROUND_STATES := [
+	State.IDLE, State.RUNNING, State.LANDING,
+	State.ATTACK_1, State.ATTACK_2, State.ATTACK_3,
+]
 const RUN_SPEED := 160.0
 const FLOOR_ACCELERATION := RUN_SPEED / 0.2
 const AIR_ACCELERATION := RUN_SPEED / 0.1
 const JUMP_VELOCITY := -320.0
 const WALL_JUMP_VELOCITY := Vector2(380, -280)
 
+@export var can_combo := false
+
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
+var is_combo_requested := false
 
 @onready var graphics: Node2D = $Graphics
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -37,6 +47,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		jump_request_timer.stop()
 		if velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
+	
+	if event.is_action_pressed("attack") and can_combo:
+		is_combo_requested = true
 
 
 func tick_physics(state: State, delta: float) -> void:
@@ -66,6 +79,9 @@ func tick_physics(state: State, delta: float) -> void:
 				graphics.scale.x = get_wall_normal().x
 			else:
 				move(default_gravity, delta)
+		
+		State.ATTACK_1, State.ATTACK_2, State.ATTACK_3:
+			stand(default_gravity, delta)
 	
 	is_first_tick = false
 	
@@ -100,19 +116,22 @@ func get_next_state(state: State) -> State:
 	if should_jump:
 		return State.JUMP
 	
+	if state in GROUND_STATES and not is_on_floor():
+		return State.FALL
+	
 	var direction := Input.get_axis("move_left", "move_right")
 	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
 	
 	match state:
 		State.IDLE:
-			if not is_on_floor():
-				return State.FALL
+			if Input.is_action_just_pressed("attack"):
+				return State.ATTACK_1
 			if not is_still:
 				return State.RUNNING
 		
 		State.RUNNING:
-			if not is_on_floor():
-				return State.FALL
+			if Input.is_action_just_pressed("attack"):
+				return State.ATTACK_1
 			if is_still:
 				return State.IDLE
 		
@@ -145,6 +164,18 @@ func get_next_state(state: State) -> State:
 				return State.WALL_SLIDING
 			if velocity.y >= 0:
 				return State.FALL
+		
+		State.ATTACK_1:
+			if not animation_player.is_playing():
+				return State.ATTACK_2 if is_combo_requested else State.IDLE
+		
+		State.ATTACK_2:
+			if not animation_player.is_playing():
+				return State.ATTACK_3 if is_combo_requested else State.IDLE
+		
+		State.ATTACK_3:
+			if not animation_player.is_playing():
+				return State.IDLE
 	
 	return state
 
@@ -188,5 +219,17 @@ func transition_state(from: State, to: State) -> void:
 			velocity = WALL_JUMP_VELOCITY
 			velocity.x *= get_wall_normal().x
 			jump_request_timer.stop()
+		
+		State.ATTACK_1:
+			animation_player.play("attack_1")
+			is_combo_requested = false
+		
+		State.ATTACK_2:
+			animation_player.play("attack_2")
+			is_combo_requested = false
+		
+		State.ATTACK_3:
+			animation_player.play("attack_3")
+			is_combo_requested = false
 	
 	is_first_tick = true
