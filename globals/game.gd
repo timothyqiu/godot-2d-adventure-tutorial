baa5d82d0f916b6a9b2ff68aa1ffcbd45f7b204f
Haoyu Qiu <timothyqiu32@gmail.com>
2023-12-28 16:09:06 +0800

    添加淡入淡出

--- globals/game.gd
+++ globals/game.gd
@@ -1,10 +1,21 @@
 extends Node
 
 @onready var player_stats: Node = $PlayerStats
+@onready var color_rect: ColorRect = $ColorRect
+
+
+func _ready() -> void:
+	color_rect.color.a = 0
 
 
 func change_scene(path: String, entry_point: String) -> void:
 	var tree := get_tree()
+	tree.paused = true
+	
+	var tween := create_tween()
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween.tween_property(color_rect, "color:a", 1, 0.2)
+	await tween.finished
 	
 	tree.change_scene_to_file(path)
 	await tree.process_frame  # 4.2 以前
@@ -14,3 +25,8 @@ func change_scene(path: String, entry_point: String) -> void:
 		if node.name == entry_point:
 			tree.current_scene.update_player(node.global_position, node.direction)
 			break
+	
+	tree.paused = false
+	
+	tween = create_tween()
+	tween.tween_property(color_rect, "color:a", 0, 0.2)
