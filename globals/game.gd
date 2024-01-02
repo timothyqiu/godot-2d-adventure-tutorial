9e12bd6c596e08382ee76cd4b3359b2e5aed5929
Haoyu Qiu <timothyqiu32@gmail.com>
2024-01-02 18:51:31 +0800

    17: 如何保持场景状态

--- globals/game.gd
+++ globals/game.gd
@@ -1,5 +1,10 @@
 extends Node
 
+# 场景的名称 => {
+#   enemies_alive => [ 敌人的路径 ]
+# }
+var world_states := {}
+
 @onready var player_stats: Node = $PlayerStats
 @onready var color_rect: ColorRect = $ColorRect
 
@@ -17,9 +22,16 @@ func change_scene(path: String, entry_point: String) -> void:
 	tween.tween_property(color_rect, "color:a", 1, 0.2)
 	await tween.finished
 	
+	var old_name := tree.current_scene.scene_file_path.get_file().get_basename()
+	world_states[old_name] = tree.current_scene.to_dict()
+	
 	tree.change_scene_to_file(path)
 	await tree.process_frame  # 4.2 以前
 #	await tree.tree_changed  # 4.2 开始
+	
+	var new_name := tree.current_scene.scene_file_path.get_file().get_basename()
+	if new_name in world_states:
+		tree.current_scene.from_dict(world_states[new_name])
 	
 	for node in tree.get_nodes_in_group("entry_points"):
 		if node.name == entry_point:
--- worlds/world.gd
+++ worlds/world.gd
@@ -22,3 +22,22 @@ func update_player(pos: Vector2, direction: Player.Direction) -> void:
 	player.direction = direction
 	camera_2d.reset_smoothing()
 #	camera_2d.force_update_scroll()  # 4.2 开始
+
+
+func to_dict() -> Dictionary:
+	var enemies_alive := []
+	for node in get_tree().get_nodes_in_group("enemies"):
+		var path := get_path_to(node)
+		enemies_alive.append(path)
+	
+	return {
+		enemies_alive=enemies_alive,
+	}
+
+
+func from_dict(dict: Dictionary) -> void:
+	for node in get_tree().get_nodes_in_group("enemies"):
+		var path := get_path_to(node)
+		if path not in dict.enemies_alive:
+			node.queue_free()
+	
